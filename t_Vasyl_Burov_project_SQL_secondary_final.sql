168f0892035633a6236c0550fbe8ba69ba728dd3
vasburov <vasylburov@gmail.com>
2024-10-24 13:03:50 +0200

    Add files via upload

--- t_Vasyl_Burov_project_SQL_secondary_final.sql
+++ t_Vasyl_Burov_project_SQL_secondary_final.sql
@@ -6,28 +6,28 @@ t_Vasyl_Burov_project_SQL_secondary_final
 CREATE TABLE t_Vasyl_Burov_project_SQL_secondary_final AS 
 WITH czechia AS (
 	SELECT
-    	e.year,
-    	c.country,
-    	e.population,
-    	ROUND(e.GDP) AS gdp,
-    	e.gini
+		e.year,
+		c.country,
+		e.population,
+		ROUND(e.GDP) AS gdp,
+		e.gini
 	FROM countries c
 	JOIN economies e ON c.country = e.country
 	WHERE 
-    	c.country = 'Czech Republic'
-    	AND e.year BETWEEN 2006 AND 2018
+		c.country = 'Czech Republic'
+		AND e.year BETWEEN 2006 AND 2018
 ), europe AS (
 	SELECT
-    	e.year,
-    	c.country,
-    	e.population,
-    	ROUND(e.GDP) AS gdp,
-    	e.gini
+		e.year,
+		c.country,
+		e.population,
+		ROUND(e.GDP) AS gdp,
+		e.gini
 	FROM countries c
 	JOIN economies e ON c.country = e.country
 	WHERE 
-    	c.continent = 'Europe'
-    	AND e.year BETWEEN 2006 AND 2018
+		c.continent = 'Europe'
+		AND e.year BETWEEN 2006 AND 2018
 )
 SELECT 
 	e.year,
